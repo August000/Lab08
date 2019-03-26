9cadc52d7951561b890c366f7e31e9b3fa2681fc
Augusto Batista <augusto.batista@ou.edu>
2019-03-26 11:07:57 -0500

    Final Files

--- src/TestBoardGame.java
+++ src/TestBoardGame.java
@@ -5,14 +5,21 @@ public class TestBoardGame
 {
 
     @Test
-    public void test()
+    public void testBoardGame()
     {
        BoardGame game = new BoardGame();
         
        game.addPlayer("Augusto", GamePiece.BLUE_BOOT, Location.HALL);
        game.addPlayer("Pedro", GamePiece.MAGENTA_RACER, Location.STUDY);
        
+       Assert.assertEquals(GamePiece.BLUE_BOOT,game.getPlayerGamePiece("Augusto"));
+       Assert.assertEquals(Location.HALL,game.getPlayersLocation("Augusto"));
        Assert.assertEquals("Augusto",game.getPlayerWithGamePiece(GamePiece.BLUE_BOOT));
+       
+       game.movePlayer("Augusto", Location.BALLROOM);
+       Assert.assertEquals(Location.BALLROOM,game.getPlayersLocation("Augusto"));
+    
+       //game.moveTwoPlayers(playerNames, newLocations);
     }
 
 }
